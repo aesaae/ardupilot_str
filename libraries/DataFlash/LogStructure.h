7fb3dae4f14fe00f34f9fcd91f827b3148719e57
sergio <sergio>
2016-03-01 18:09:52 +0000

    Made changes to LogStructure.h, added comment to LogStructure definition.

--- libraries/DataFlash/LogStructure.h
+++ libraries/DataFlash/LogStructure.h
@@ -17,9 +17,9 @@
 struct LogStructure {
     uint8_t msg_type;
     uint8_t msg_len;
-    //const char name[5];
+    //const char name[5]; // <- Original value
 	const char name[10];
-    //const char format[16];
+    //const char format[16]; // <- Original value
 	const char format[24];
     const char labels[64];
 };
